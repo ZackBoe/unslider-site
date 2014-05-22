e0acf362b9296ef930dbfda0cb532f034eb60f10
Visual Idiot <iam@visualidiot.com>
2014-05-22 19:34:28 +0100

    updates

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php function size($wut) { echo file_get_contents('http://localhost:1001/download/' . $wut . '.size'); } ?><!DOCTYPE html>
+<?php function size($wut) { echo file_get_contents('http://localhost:1001/build/' . $wut . '.size'); } ?><!DOCTYPE html>
 <html class="no-js">
     <head>
         <meta charset="utf-8">
@@ -37,12 +37,20 @@
                 <h2>Installing Unslider</h2>
                 
                 <div class="instructions">
-                	<ol role="navigation">
-                		<li class="active">1</li>
-                		<li>2</li>
-                		<li>3</li>
-                		<li>4</li>
-                	</ol>
+                	<ul>
+                		<li>
+                			<h3>Download jQuery, Unslider and any extras.</h3>
+                			<p>If you’re not already using jQuery (you can check by running <code>!!jQuery</code> in the console), you need to add a link to jQuery in your page’s source code.</p>
+                			<p>When you’ve done that, you’re ready to add Unslider. Just add the Unslider <code>&lt;script&gt;</code> element as well. I you’re using any <a href="#extras">Extras</a>, you need to change the URL accordingly.</p>
+                			
+                			<pre>&lt;script src="//code.jquery.com/jquery-latest.min.js"&gt;&lt;/script&gt; &lt;!-- jQuery --&gt;
+&lt;script src="//unslider.com/build/standard.min"&gt;&lt;/script&gt; &lt;!-- Unslider, made a bit smaller --&gt;</pre>
+                		</li>
+                		
+                		<li>
+                			<h3>another slide</h3>
+                		</li>
+                	</ul>
                 </div>
             </div>
         </section>
@@ -53,32 +61,35 @@
 	                <h2>The Unslider options object</h2>
 	                <p>There’s a few configurable points you can change when setting Unslider up, which you can easily add by using an <b>options object</b>.</p>
 	                <p>You can use as many (or as few) options as you want, or you can leave the whole options object blank.</p>
-	                <p>If you’re going to use an object, make sure to wrap it in <b>{curly braces}</b>, otherwise you’ll just get a broken page.</p>
+	                <p>If you’re going to use an object, make sure to wrap it in <code>{curly braces}</code>, otherwise you’ll just get a broken page.</p>
 				</div>
                 
-                <pre>{
-    <span class="comment">//  How fast to move between slides (in ms)</span>
-    speed: <span class="val">500</span>,
-    
-    <span class="comment">//  The length of time to leave between moving slides (in ms)</span>
-    interval: <span class="val">2000</span>,
-    
-    <span class="comment">//  Function that gets called when the slide’s done</span>
-    complete: <span class="val">function(el, index) {}</span>,
+                <pre><span class="lite">&lt;script&gt;
+    $('.banner').unslider(</span>{
+        <span class="comment">//  How fast to move between slides (in ms)</span>
+        speed: <span class="val">500</span>,
+	    
+        <span class="comment">//  The length of time to leave between moving slides (in ms)</span>
+        interval: <span class="val">2000</span>,
 
-    <span class="comment">//  Add keyboard (L/R arrow) support?</span>
-    keys: <span class="val">true</span>,
-    
-    <span class="comment">//  Add navigation dots?
-    //  This will only append a &lt;nav&gt; element with &lt;ol&gt; in.
-    //  If you want to style the dots, you'll need to write
-    //  some custom CSS to do that. It doesn't come included with
-    //  Unslider, to keep things small.</span>
-    dots: <span class="val">true</span>,
-    
-    <span class="comment">//  Want the slider to start automatically? We can do that.</span>
-    autostart: <span class="val">true</span>
-}</pre>
+        <span class="comment">//  Function that gets called when the slide’s done</span>
+        complete: <span class="val">function(el, index) {}</span>,
+
+        <span class="comment">//  Add keyboard (L/R arrow) support?</span>
+        keys: <span class="val">true</span>,
+
+        <span class="comment">//  jQuery animation easing</span>
+        easing: <span class="val">'swing'</span>,
+
+        <span class="comment">//  Add navigation dots?
+        //  true for yes, false for no
+        //  or custom jQuery selector chooses where to insert</span>
+        dots: <span class="val">true</span>,
+
+        <span class="comment">//  Want the slider to start automatically? We can do that.</span>
+        autostart: <span class="val">true</span>
+    }<span class="lite">);
+&lt;/script&gt;</span></pre>
             </div>
         </section>
         
@@ -103,8 +114,8 @@
                         <p>Add <code>{ infinite: true }</code> to your Unslider’s options object to activate the Infinite Scroll extra.</p>
                         
                         <p class="download">
-                            <a class="btn subdued" href="/download/infinite">Unslider + Infinite<span>standard, <?php size('infinite'); ?></span></a>
-                            <a class="btn" href="/download/infinite.min">Unslider + Infinite <span>minified, <?php size('infinite.min'); ?></span></a>
+                            <a class="btn subdued" href="/build/infinite">Unslider + Infinite<span>standard, <?php size('infinite'); ?></span></a>
+                            <a class="btn" href="/build/infinite.min">Unslider + Infinite <span>minified, <?php size('infinite.min'); ?></span></a>
                         </p>
                     </li>
                     
@@ -124,8 +135,8 @@
                         <p>Add <code>{ fade: true }</code> to your Unslider’s options object to activate the Fade Transition extra.</p>
                         
                         <p class="download">
-                            <a class="btn subdued" href="/download/fade">Unslider + Fade <span>standard, <?php size('fade'); ?></span></a>
-                            <a class="btn" href="/download/fade.min">Unslider + Fade <span>minified, <?php size('fade.min'); ?></span></a>
+                            <a class="btn subdued" href="/build/fade">Unslider + Fade <span>standard, <?php size('fade'); ?></span></a>
+                            <a class="btn" href="/build/fade.min">Unslider + Fade <span>minified, <?php size('fade.min'); ?></span></a>
                         </p>
                     </li>
                     
@@ -138,15 +149,15 @@
                                 <li>Keep on runnin’</li>
                                 <li>Keep on hidin’</li>
                                 <li class="halfling">One fine day I’m gonna be the one to make you understand</li>
-                                <li>Infinite scroll is the man</li>
+                                <li>Vertical scroll is the bees knees</li>
                             </ul>
                         </div>
                         
                         <p>Add <code>{ vertical: true }</code> to your Unslider’s options object to activate the Infinite Scroll extra.</p>
 
                         <p class="download">
-                            <a class="btn subdued" href="/download/vertical">Unslider + Vertical <span>standard, <?php size('vertical'); ?></span></a>
-                            <a class="btn" href="/download/vertical.min">Unslider + Vertical <span>minified, <?php size('vertical.min'); ?></span></a>
+                            <a class="btn subdued" href="/build/vertical">Unslider + Vertical <span>standard, <?php size('vertical'); ?></span></a>
+                            <a class="btn" href="/build/vertical.min">Unslider + Vertical <span>minified, <?php size('vertical.min'); ?></span></a>
                         </p>
                     </li>
                 </ul>
@@ -157,8 +168,8 @@
             <div class="wrap">
             	<h2>Download Unslider</h2>
             	
-	            <a class="btn" href="/download/standard">Download <span>standard, <?php size('standard'); ?></span></a>
-	            <a class="btn hilite" href="/download/standard.min">Download <span>minified, <?php size('standard.min'); ?></span></a>
+	            <a class="btn" href="/build/standard">Download <span>standard, <?php size('standard'); ?></span></a>
+	            <a class="btn hilite" href="/build/standard.min">Download <span>minified, <?php size('standard.min'); ?></span></a>
 	            
 	            <aside class="social">
 	            	<h2>Unslider elsewhere</h2>
@@ -173,7 +184,7 @@
         <script src="//code.jquery.com/jquery-latest.min.js"></script>
         
         <!-- Our Unslider script, lets the bare minimum work. -->
-        <script src="/unslider/src/unslider.js"></script>
+        <script src="//localhost:1000/src/unslider.js"></script>
         
         <!-- Optional extras: you don't need these for Unslider to work, but if you want infinite/fade/vertical extras, add 'em in. -->
         <script src="/unslider/src/extras/unslider.infinite.js"></script>
@@ -186,6 +197,19 @@
             	//  Not actually used on the site, but this is the default functionality
                 //  Default demo, no special features
                 $('.slider').unslider();
+                
+                //  Our "instructions" slider
+                //  Doesn't autoscroll
+                $('.instructions').unslider({
+                	dots: $('#install .wrap'),
+                	autostart: false,
+                	keys: true,
+                	speed: 250,
+                	infinite: true
+                });
+                
+                //  Disable keyboard support on any other slides
+                $.Unslider.defaults.keys = false;
                 
                 //  Infinite scroll
                 //  Only works if you've got the unslider.infinite.js extra on the page too
